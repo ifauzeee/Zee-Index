fb40dae541e7308d7ae41c506e744f0164c4370d
ifauzeee <ifauze343@gmail.com>
2025-12-12 19:47:42 +0700

    fix: remove hardcoded cookie config to use default secure session naming

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -177,15 +177,5 @@ export const authOptions: AuthOptions = {
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
-  cookies: {
-    sessionToken: {
-      name: `next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: process.env.NODE_ENV === "production",
-      },
-    },
-  },
+
 };
