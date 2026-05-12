b1abb7223c1e7397ff17f9b9d83accf30b25d1ec
ifauzeee <ifauze343@gmail.com>
2026-05-12 22:24:13 +0700

    fix: redirect to root if already configured and accessing /setup

--- middleware.ts
+++ middleware.ts
@@ -93,6 +93,7 @@ export default async function middleware(request: NextRequest) {
   }
 
   if (isConfigured && pathnameWithoutLocale.startsWith("/setup")) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   if (
